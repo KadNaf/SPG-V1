b8b31fc3e714dcddfe4ca75cb51a41f802e1baa8
Oumougou Naffiou KADIRI <konaffiou01@expresso.local>
2026-08-18 01:09:29 +0000

    Description des modifications

--- R/ui_isolation_by_distance.R
+++ R/ui_isolation_by_distance.R
@@ -17,10 +17,8 @@ isolation_by_distance_UI <- function(id) {
     module_banner(
       "atom",
       "Isolation by Distance · Mantel Test",
-      "<br>",
-      "One-sided p-value = (b+1)/(m+1) (bias-corrected proportion)",
-      "<br>",
-      "One-sided p-value = (b/m) (plain proportion, no correction)",
+      "One-sided p-value = (b+1)/(m+1) (bias-corrected proportion)<br>
+      One-sided p-value = b/m (plain proportion, no correction)",
       "#0c4a6e"
     ),
 
